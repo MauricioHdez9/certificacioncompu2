9147b52ac6bc076eb3c5ad7108dbdde3ad630e82
tuxryu <shiwarrokur@gmail.com>
2021-12-16 13:55:13 -0600

    subiendo pagina web

---
+++ js/nombre.js
@@ -0,0 +1,9 @@
+let num // num= numero
+num = prompt('ingresa tu nombre');
+console.log(num.length)
+if(num.length !=0)
+{
+    console.log( "entro",num.length)
+    prompt("hola bienvenido a mi pagina",num);
+    console.log( "salio",num.length)
+}
